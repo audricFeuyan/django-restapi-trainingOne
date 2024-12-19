9c6d81f942273994bc90ec4f74478732fc014ab2
FEUYAN TCHOUO AUDRIC CAUDIN <audric.feuyan@mboadigital.tech>
2024-12-19 04:47:46 +0100

    Snippets list and details endpoints

---
+++ snippets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetList.as_view())
+]
+
+urlpatters = format_suffix_patterns(urlpatterns)
